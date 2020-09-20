cbd047e40b380bcf29d3946aed91b764c7e9638a
azizka <zizka.alexander@gmail.com>
2020-09-20 23:15:20 +0200

    data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,25 @@
 ## code to prepare `DATASET` dataset goes here
 
 usethis::use_data(DATASET, overwrite = TRUE)
+library(readxl)
+library(tidyverse)
+
+dat <- read_excel("data-raw/result_table_systematic_review.xlsx")
+
+traits <- dat %>% 
+  select(author, year, akronym, method.name, scale, implementation.standard,
+         contains("scope"), contains("includes"),
+         free.text.description, `DOI/link`) %>% 
+  mutate(ID = paste(author, year, sep =""))
+
+use_data(traits)
+
+#also add literature lsit
+library(bib2df)
+
+literature1 <- bib2df("data-raw/syst_review.bib")
+literature2 <- bib2df("data-raw/apriori.bib")
+
+literature <- bind_rows(literature1, literature2)
+
+use_data(literature)
